5f9ec50f498fd586a43bb11574cc8c8b19cb0338
shark <sharkshen@outlook.com>
2019-09-21 02:51:56 +0800

    Initial commit

---
+++ src/main/java/com/linkjb/chaochao/ChaochaoApplication.java
@@ -0,0 +1,13 @@
+package com.linkjb.chaochao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaochaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaochaoApplication.class, args);
+    }
+
+}
